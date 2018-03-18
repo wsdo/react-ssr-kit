cde2defeae4bdb1cc0910c301042af990fff8344
wangshudong <wsd312@163.com>
2018-03-18 13:34:39 +0800

    add ssr

---
+++ build/webpack.config.client.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const HTMlPlugin = require('html-webpack-plugin')
+module.exports = {
+    entry: {
+        app: path.join (__dirname, '../client/app.js')
+    },
+    output: {
+        filename: '[name].[hash].js',
+        path: path.join(__dirname,'../dist'),
+        publicPath: '/public'
+    },
+    module: {
+        rules: [
+            {
+                test: /.jsx$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /.js$/,
+                loader: 'babel-loader',
+                exclude: [
+                    path.join(__dirname, '../node_modules')
+                ]
+            }
+        ]
+    },
+    plugins:[
+        new HTMlPlugin({
+            template:path.join(__dirname, "../client/template.html")
+        })
+    ]
+}
---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const ReactSSR = require('react-dom/server')
+const serverEntry = require('../dist/server-entry').dafault
+const fs = require('fs')
+const path = require('path')
+const template = fs.readFileSync(path.join(__dirname, '../dist/index.html'),'utf8')
+const app = express()
+
+
+console.log(serverEntry);
+
+app.use('/public',express.static(path.join(__dirname, '../dist')))
+app.get('*',function (req,res) {
+    const appString = ReactSSR.renderToString(serverEntry)
+    console.log(11,appString);
+    
+    res.send(template.replace('<app></app>', appString))
+})
+
+app.listen(3333, function () {
+    console.log("server is listening on 3333")
+})
