3ab3bcecb43fe7799321f060d719d32b76ddaba4
王树冬 <wangshudong@hujiang.com>
2018-06-29 18:48:15 +0800

    增加 webpack marege

--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -1,9 +1,12 @@
 const path = require('path')
-const HTMlPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
+const baseConfig = require('./webpack.base')
 const isDev = process.env.NODE_ENV === 'development'
+const webpackMerge = require('webpack-merge')
+const HTMLPlugin = require('html-webpack-plugin')
 
-const config = {
+
+const config = webpackMerge(baseConfig, {
   entry: {
     app: path.join(__dirname, '../client/app.js')
   },
@@ -12,28 +15,12 @@ const config = {
     path: path.join(__dirname, '../dist'),
     publicPath: '/public/'
   },
-  module: {
-    rules: [
-      {
-        test: /.jsx$/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /.js$/,
-        loader: 'babel-loader',
-        exclude: [
-          path.join(__dirname, '../node_modules')
-        ]
-      }
-    ]
-  },
   plugins: [
-    new HTMlPlugin({
+    new HTMLPlugin({
       template: path.join(__dirname, '../client/template.html')
     })
   ]
-}
-
+})
 if (isDev) {
   config.entry = {
     app: [
--- build/webpack.config.server.js
+++ build/webpack.config.server.js
@@ -1,28 +1,15 @@
 const path = require('path')
-module.exports = {
+const webpackMerge = require('webpack-merge')
+const baseConfig = require('./webpack.base')
+
+module.exports = webpackMerge(baseConfig, {
   target: 'node',
   entry: {
     app: path.join(__dirname, '../client/server-entry.js')
   },
+  externals: Object.keys(require('../package.json').dependencies),
   output: {
     filename: 'server-entry.js',
-    path: path.join(__dirname, '../dist'),
-    publicPath: '/public',
     libraryTarget: 'commonjs2'
-  },
-  module: {
-    rules: [
-      {
-        test: /.jsx$/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /.js$/,
-        loader: 'babel-loader',
-        exclude: [
-          path.join(__dirname, '../node_modules')
-        ]
-      }
-    ]
   }
-}
+})
